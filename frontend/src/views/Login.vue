e7766abbb709d81e8d180131bdcaa00bbb2e12a3
jb-delafosse <jean-baptiste@lumapps.com>
2021-05-21 23:08:20 +0200

    feat(front): add Login view

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-content>
+    <v-btn class="ma-2" color="primary" dark>
+      Login with google
+      <v-icon dark right>
+        mdi-account
+      </v-icon>
+    </v-btn>
+  </v-content>
+</template>
+
+<script>
+export default {
+  name: "Login.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
