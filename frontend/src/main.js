962e7d3fda1cb6f08398814ed316de236c4debb7
jb-delafosse <jean-baptiste@lumapps.com>
2021-05-30 10:09:11 +0200

    feat(gauth): add gauth in root

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import GAuth from 'vue-google-oauth2';
+
+
+const gAuthOption = {
+  clientId: '87641415248-t47sil8hkbrrfp514n6oh9avbvg3jrqv.apps.googleusercontent.com',
+  scope: 'email',
+  prompt: 'consent',
+  fetch_basic_profile: true,
+};
 
 Vue.config.productionTip = false
+
+Vue.use(GAuth, gAuthOption);
 
 new Vue({
   router,
